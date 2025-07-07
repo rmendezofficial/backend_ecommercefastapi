fc6f2a21dfb55ad76c35e02eba92ec5397c30c56
RicardoMendezPinero <ricgabmendez2007@gmail.com>
2025-07-07 11:39:56 -0500

    fixed timezone problem with database

--- database.py
+++ database.py
@@ -11,7 +11,7 @@ engine=create_engine(URL_DATABASE)
 @event.listens_for(engine, "connect")
 def set_timezone(dbapi_connection, connection_record):
     cursor = dbapi_connection.cursor()
-    cursor.execute("SET time_zone = '+00:00';")  # UTC offset
+    cursor.execute("SET timezone = 'UTC';")  # UTC offset
     cursor.close()
 
 SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
